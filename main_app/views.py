ef3fad98ce5d4be4ca34f4294d5d58ecb97756af
Nova Hasley <supernova@Novas-MacBook-Pro.local>
2019-09-12 13:42:09 -0500

    home.html added and css file

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 
-# Add the following import
-from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Test</h1>')
+  return render(request, 'home.html')
