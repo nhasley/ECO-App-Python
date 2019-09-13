306619085fa4c516c139ff243cd800d7118bd32c
Emily Landgrebe <emilandgrebe@gmail.com>
2019-09-13 11:43:40 -0500

    css minor fix

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('/challenges', views.challenges, name='challenges'),
-  path('/profile', views.profile, name='profile'),
+  path('challenges/', views.challenges, name='challenges'),
+  path('profile/', views.profile, name='profile'),
 ]
