391f7fbc1085f62660794cd2ebf9b3f810ebb3ea
Nova Hasley <supernova@Novas-MacBook-Pro.local>
2019-09-13 13:11:02 -0500

    urls update

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,11 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
   path('', views.challenges, name='challenges'),
-=======
-  path('', views.home, name='home'),
-  path('challenges/', views.challenges, name='challenges'),
->>>>>>> 306619085fa4c516c139ff243cd800d7118bd32c
   path('profile/', views.profile, name='profile'),
 ]
