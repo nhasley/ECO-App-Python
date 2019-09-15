75f972a30dfe9ba9ad2506d7ffc893cf4830f098
Nova Hasley <supernova@Novas-MBP.lan1>
2019-09-15 11:34:54 -0500

    added admin database

--- main_app/models.py
+++ main_app/models.py
@@ -2,4 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Challenge(models.Model):
+    name = models.CharField(max_length=100)
+    points = models.IntegerField()
+    description = models.CharField(max_length=250)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- main_app/views.py
+++ main_app/views.py
@@ -1,17 +1,9 @@
 from django.shortcuts import render
-
-class Challenge:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, points, description):
-    self.name = name
-    self.points = points
-    self.description = description
-
-challenges = [
-  Challenge('Bike', 10, 'Take a pic'),
-]
+from .models import Challenge
 
 # Define the home view
 def challenges_index(request):
+    challenges = Challenge.objects.all()
     return render(request, 'challenges.html', {'challenges': challenges})
 
 def profile(request):
