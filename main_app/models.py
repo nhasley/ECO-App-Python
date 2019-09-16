204c2d3ce8749e673a39daec9428c1c38fddcd1d
Your Name <supernova@Novas-MacBook-Pro.local>
2019-09-16 10:42:31 -0500

    Adding photo to models, urls, views

--- main_app/models.py
+++ main_app/models.py
@@ -14,3 +14,10 @@ class Post(models.Model):
         default='Metro'
     )
     description = models.CharField(max_length=100)
+
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for post_id: {self.post_id} @{self.url}"
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Post 
-# from .forms import PostForm
+from .models import Post, Photo
 import uuid
 import boto3
 
